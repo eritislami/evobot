2c01ecc6a1f1d237ddc4e3898c316c60c0894e94
Erit Islami <eritislami@gmail.com>
2019-05-23 22:59:52 +0200

    Feature: Remove command Removes a song from the queue using its index number Ex: /remove 5 Removes the 5th song in the queue

---
+++ commands/remove.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "remove",
+  description: "Remove song from the queue",
+  async execute(message, args) {
+    if (!args.length) return message.reply("Usage: /remove <Queue Number>")
+    const serverQueue = message.client.queue.get(message.guild.id)
+    if (!serverQueue) return message.channel.send("There is no queue.").catch(console.error)
+
+    const song = serverQueue.songs.splice((args[0] - 1), 1)
+    serverQueue.textChannel.send(`${message.author} ❌ removed **${song[0].title}** from the queue.`)
+  }
+}
