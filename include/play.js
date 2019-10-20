6826e2520fe2532adca74569d2e15a2c9f857005
Erit Islami <eritislami@gmail.com>
2019-10-20 19:42:13 +0200

    Keep media control reactions for 30 minutes

--- include/play.js
+++ include/play.js
@@ -1,93 +1,96 @@
-const ytdlDiscord = require("ytdl-core-discord")
+const ytdlDiscord = require("ytdl-core-discord");
 
 module.exports = {
   async play(song, message) {
-    const queue = message.client.queue.get(message.guild.id)
+    const queue = message.client.queue.get(message.guild.id);
 
     if (!song) {
-      queue.channel.leave()
-      message.client.queue.delete(message.guild.id)
-      return queue.textChannel.send("🚫 Music queue ended.").catch(console.error)
+      queue.channel.leave();
+      message.client.queue.delete(message.guild.id);
+      return queue.textChannel.send("🚫 Music queue ended.").catch(console.error);
     }
 
     try {
-      var stream = await ytdlDiscord(song.url, { filter: "audioonly", quality: "highestaudio" })
+      var stream = await ytdlDiscord(song.url, { filter: "audioonly", quality: "highestaudio" });
     } catch (error) {
       if (queue) {
-        queue.songs.shift()
-        module.exports.play(queue.songs[0], message)
+        queue.songs.shift();
+        module.exports.play(queue.songs[0], message);
       }
 
       if (error.message.includes("copyright")) {
-        return message.channel.send("⛔ A video could not be played due to copyright protection ⛔").catch(console.error)
+        return message.channel
+          .send("⛔ A video could not be played due to copyright protection ⛔")
+          .catch(console.error);
       } else {
-        console.error(error)
+        console.error(error);
       }
     }
 
-    const dispatcher = queue.connection.play(stream, { type: "opus", passes: 3 })
+    const dispatcher = queue.connection
+      .play(stream, { type: "opus", passes: 3 })
       .on("end", () => {
         // Recursively play the next song
-        queue.songs.shift()
-        module.exports.play(queue.songs[0], message)
+        queue.songs.shift();
+        module.exports.play(queue.songs[0], message);
       })
-      .on("error", console.error)
-    dispatcher.setVolumeLogarithmic(queue.volume / 100)
+      .on("error", console.error);
+    dispatcher.setVolumeLogarithmic(queue.volume / 100);
 
     try {
-      var playingMessage = await queue.textChannel.send(`🎶 Started playing: **${song.title}** ${song.url}`)
-      await playingMessage.react("⏭")
-      await playingMessage.react("⏸")
-      await playingMessage.react("▶")
-      await playingMessage.react("⏹")
+      var playingMessage = await queue.textChannel.send(`🎶 Started playing: **${song.title}** ${song.url}`);
+      await playingMessage.react("⏭");
+      await playingMessage.react("⏸");
+      await playingMessage.react("▶");
+      await playingMessage.react("⏹");
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
 
-    const filter = (reaction, user) => user.id !== message.client.user.id
-    const collector = playingMessage.createReactionCollector(filter, { time: song.duration > 0 ? (song.duration * 1000) : 300000 })
+    const filter = (reaction, user) => user.id !== message.client.user.id;
+    const collector = playingMessage.createReactionCollector(filter, { time: 1800000 });
 
     collector.on("collect", (reaction, user) => {
       // Stop if there is no queue on the server
-      if (!queue) return
+      if (!queue) return;
 
       switch (reaction.emoji.name) {
         case "⏭":
-          queue.connection.dispatcher.end()
-          queue.textChannel.send(`${user} ⏩ skipped the song`).catch(console.error)
-          collector.stop()
-          playingMessage.reactions.removeAll()
+          queue.connection.dispatcher.end();
+          queue.textChannel.send(`${user} ⏩ skipped the song`).catch(console.error);
+          collector.stop();
+          playingMessage.reactions.removeAll();
           break;
 
         case "⏸":
           if (!queue.playing) break;
-          queue.playing = false
-          queue.connection.dispatcher.pause()
-          queue.textChannel.send(`${user} ⏸ paused the music.`).catch(console.error)
+          queue.playing = false;
+          queue.connection.dispatcher.pause();
+          queue.textChannel.send(`${user} ⏸ paused the music.`).catch(console.error);
           break;
 
         case "▶":
           if (queue.playing) break;
-          queue.playing = true
-          queue.connection.dispatcher.resume()
-          queue.textChannel.send(`${user} ▶ resumed the music!`).catch(console.error)
+          queue.playing = true;
+          queue.connection.dispatcher.resume();
+          queue.textChannel.send(`${user} ▶ resumed the music!`).catch(console.error);
           break;
 
         case "⏹":
-          queue.songs = []
-          queue.connection.dispatcher.end()
-          queue.textChannel.send(`${user} ⏹ stopped the music!`).catch(console.error)
-          collector.stop()
-          playingMessage.reactions.removeAll()
+          queue.songs = [];
+          queue.connection.dispatcher.end();
+          queue.textChannel.send(`${user} ⏹ stopped the music!`).catch(console.error);
+          collector.stop();
+          playingMessage.reactions.removeAll();
           break;
 
         default:
           break;
       }
-    })
+    });
 
     collector.on("end", () => {
-      playingMessage.reactions.removeAll()
-    })
+      playingMessage.reactions.removeAll();
+    });
   }
-}
+};
