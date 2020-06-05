a4e083c2bf104676945f4fd8b30cb8bd9752c997
Alhassan Raad <23234466+alhassanv@users.noreply.github.com>
2020-06-05 10:53:32 +0300

    Make multiple embeds if queue is too long

--- commands/queue.js
+++ commands/queue.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, splitMessage } = require("discord.js");
 
 module.exports = {
   name: "queue",
@@ -7,16 +7,18 @@ module.exports = {
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!serverQueue) return message.reply("There is nothing playing.").catch(console.error);
+    const description = serverQueue.songs.map((song, index) => `${index + 1}. ${song.title}`);
 
     let queueEmbed = new MessageEmbed()
-      .setTitle("EvoBot Music Queue")
-      .setDescription(serverQueue.songs.map((song, index) => `${index + 1}. ${song.title}`))
-      .setColor("#F8AA2A");
+    .setTitle("EvoBot Music Queue")
+    .setDescription(serverQueue.songs.map((song, index) => `${index + 1}. ${song.title}`))
+    .setColor("#F8AA2A");
 
-    queueEmbed.setTimestamp();
-    if (queueEmbed.description.length >= 2048)
-        queueEmbed.description =
-          queueEmbed.description.substr(0, 2007) + "\nQueue is larger than character limit...";
-    return message.channel.send(queueEmbed);
+    const splitDescription = splitMessage(description, { maxLength: 2048, char: '\n', prepend: '', append: '' });
+	splitDescription.forEach(async m => {
+        queueEmbed.setDescription(m);
+        message.channel.send(queueEmbed);
+	});
+
   }
 };
