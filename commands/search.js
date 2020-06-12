c92fe53c4375339c65f4fc953b3e6f1bc0e08f7c
Erit Islami <eritislami@gmail.com>
2020-06-12 11:05:43 +0200

    Feature: add search

---
+++ commands/search.js
@@ -0,0 +1,44 @@
+const { MessageEmbed } = require("discord.js");
+const { YOUTUBE_API_KEY } = require("../config.json");
+const YouTubeAPI = require("simple-youtube-api");
+const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
+
+module.exports = {
+  name: "search",
+  description: "Search and select videos to play",
+  async execute(message, args) {
+    if (message.channel.activeCollector)
+      return message.reply("A message collector is already active in this channel.");
+    if (!message.member.voice.channel)
+      return message.reply("You need to join a voice channel first!").catch(console.error);
+
+    const search = args.join(" ");
+
+    let resultsEmbed = new MessageEmbed()
+      .setTitle(`**Reply with the song number you want to play**`)
+      .setDescription(`Results for: ${search}`)
+      .setColor("#F8AA2A");
+
+    try {
+      const results = await youtube.searchVideos(search, 10);
+      results.map((video, index) => resultsEmbed.addField(video.shortURL, `${index + 1}. ${video.title}`));
+
+      var resultsMessage = await message.channel.send(resultsEmbed);
+
+      function filter(msg) {
+        const pattern = /(^[1-9][0-9]{0,1}$)/g;
+        return pattern.test(msg.content) && parseInt(msg.content.match(pattern)[0]) <= 10;
+      }
+
+      message.channel.activeCollector = true;
+      const response = await message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] });
+      const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
+
+      message.client.commands.get("play").execute(message, [choice]);
+      resultsMessage.delete().catch(console.error);
+    } catch (error) {
+      console.error(error);
+      message.channel.activeCollector = false;
+    }
+  }
+};
