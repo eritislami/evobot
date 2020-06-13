b0077d265a93eb08d7dbb15a664110e4e73ed8dc
Erit Islami <eritislami@gmail.com>
2020-06-13 21:07:34 +0200

    Prevent search without argument

--- commands/search.js
+++ commands/search.js
@@ -7,6 +7,8 @@ module.exports = {
   name: "search",
   description: "Search and select videos to play",
   async execute(message, args) {
+    if (!args.length)
+      return message.reply(`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`).catch(console.error);
     if (message.channel.activeCollector)
       return message.reply("A message collector is already active in this channel.");
     if (!message.member.voice.channel)
@@ -33,7 +35,7 @@ module.exports = {
       message.channel.activeCollector = true;
       const response = await message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] });
       const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
-      
+
       message.channel.activeCollector = false;
       message.client.commands.get("play").execute(message, [choice]);
       resultsMessage.delete().catch(console.error);
