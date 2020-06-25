20b6f9f83e06b437d5e89b66405fa58cb311fa4f
Matthew Gray <matttgray179@gmail.com>
2020-06-25 21:25:39 +1200

    add permission check to prefix command

--- commands/prefix.js
+++ commands/prefix.js
@@ -7,8 +7,11 @@ module.exports = {
   aliases: ["pf"],
   description: "Change the bot\'s prefix",
   async execute(message, args) {
-    // if there's at least one argument, set the prefix
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send(`You need to be an administrator to do this ${message.author}`)
+    }
     if (args.length) {
+      // if there's at least one argument, set the prefix
       await prefixes.set(message.guild.id, args[0]);
       return message.channel.send(`Successfully set prefix to \`${args[0]}\``);
     }
