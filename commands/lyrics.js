a8f4c134b6788e62fc625a75bec2a1fe01bc8bfe
Erit Islami <eritislami@gmail.com>
2020-07-03 10:13:46 +0200

    Lyrics error handling

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -9,14 +9,22 @@ module.exports = {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) return message.channel.send("There is nothing playing.").catch(console.error);
 
+    let lyrics = null;
+
+    try {
+      lyrics = await lyricsFinder(queue.songs[0].title, "");
+    } catch (error) {
+      lyrics = `No lyrics found for ${queue.songs[0].title}.`;
+    }
+
     let lyricsEmbed = new MessageEmbed()
       .setTitle("Lyrics")
-      .setDescription(
-        (await lyricsFinder(queue.songs[0].title, "")) || `No lyrics found for ${queue.songs[0].title}`
-      )
+      .setDescription(lyrics)
       .setColor("#F8AA2A")
       .setTimestamp();
 
+    if (lyricsEmbed.description.length >= 2048)
+      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
     return message.channel.send(lyricsEmbed).catch(console.error);
   }
 };
