1e6fcc26b7789cda75b2b90f3349ef173316f6ab
Erit Islami <eritislami@gmail.com>
2020-07-29 17:25:23 +0200

    fix(play): add fallback for non opus sc streams
    fix #140

--- include/play.js
+++ include/play.js
@@ -14,12 +14,18 @@ module.exports = {
     }
 
     let stream = null;
+    let streamType = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
 
     try {
       if (song.url.includes("youtube.com")) {
         stream = await ytdlDiscord(song.url, { highWaterMark: 1 << 25 });
-      } else if (song.url.includes("soundcloud.com") && SOUNDCLOUD_CLIENT_ID) {
-        stream = await scdl.downloadFormat(song.url, scdl.FORMATS.OPUS, SOUNDCLOUD_CLIENT_ID);
+      } else if (song.url.includes("soundcloud.com")) {
+        try {
+          stream = await scdl.downloadFormat(song.url, scdl.FORMATS.OPUS, SOUNDCLOUD_CLIENT_ID ? SOUNDCLOUD_CLIENT_ID : undefined);
+        } catch (error) {
+          stream = await scdl.downloadFormat(song.url, scdl.FORMATS.MP3, SOUNDCLOUD_CLIENT_ID ? SOUNDCLOUD_CLIENT_ID : undefined);
+          streamType = "unknown";
+        }
       }
     } catch (error) {
       if (queue) {
@@ -33,9 +39,8 @@ module.exports = {
 
     queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));
 
-    const type = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
     const dispatcher = queue.connection
-      .play(stream, { type: type })
+      .play(stream, { type: streamType })
       .on("finish", () => {
         if (collector && !collector.ended) collector.stop();
 
