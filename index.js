eb15f35f0a4d7b8fd2b0433aac0907233add0ef5
Bikey29 <59862884+Bikey29@users.noreply.github.com>
2020-08-01 18:38:03 +0530

    Update index.js
    Leaving when no one in voice channel.

--- index.js
+++ index.js
@@ -81,3 +81,36 @@ client.on("message", async (message) => {
     message.reply("There was an error executing that command.").catch(console.error);
   }
 });
+
+//voice stats
+client.on("voiceStateUpdate", (oldState, newState) => {
+  if (oldState.channel === null) return newState.setSelfDeaf(true);
+  let change = false;
+  const queue = client.queue.get(newState.guild.id);
+  if (!queue) return;
+  if (newState.member.id === client.user.id && oldState.member.id === client.user.id) {
+    if (newState.member.voice.channel === null) {
+      queue.songs = [];
+      queue.looping = false;
+      queue.endReason = "manual disconnect";
+      return client.queue.delete(newState.guild.id);
+    }
+    if (newState.member.voice.channel !== queue.voiceChannel) {
+      change = true;
+      queue.voiceChannel = newState.member.voice.channel;
+      queue.connection = newState.connection;
+    }
+  }
+  if ((oldState.channel.members.size === 1 && oldState.channel === queue.voiceChannel) || change) {
+    setTimeout(() => {
+      if (!queue || !queue.connection.dispatcher || queue.connection.dispatcher === null) return;
+      if (queue.voiceChannel.members.size === 1) {
+        queue.textChannel.send("⚠ No one in voice channel leaving..")
+        queue.songs = [];
+        queue.looping = false;
+        queue.endReason = "Timeout";
+        queue.connection.dispatcher.end();
+      }
+    }, 300000);
+  }
+});
