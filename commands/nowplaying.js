242514a268a3688f2325ca68fca6b0167241ad1d
Sparker <60593579+Sparker-99@users.noreply.github.com>
2020-08-04 14:45:41 +0530

    modified nowplaying

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,3 +1,4 @@
+const createBar = require("string-progressbar");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
@@ -7,15 +8,17 @@ module.exports = {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) return message.reply("There is nothing playing.").catch(console.error);
     const song = queue.songs[0];
+    const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
+    const left = song.duration - seek;
 
     let nowPlaying = new MessageEmbed()
       .setTitle("Now playing")
       .setDescription(`${song.title}\n${song.url}`)
       .setColor("#F8AA2A")
       .setAuthor("EvoBot")
-      .setTimestamp();
+      .addField("\u200b", new Date(seek * 1000).toISOString().substr(11, 8) + "[" + createBar((song.duration == 0 ? seek : song.duration), seek, 20)[0] + "]" + (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000).toISOString().substr(11, 8)), false);
 
-    if (song.duration > 0) nowPlaying.setFooter(new Date(song.duration * 1000).toISOString().substr(11, 8));
+    if (song.duration > 0) nowPlaying.setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
 
     return message.channel.send(nowPlaying);
   }
