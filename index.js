a20deb616cc379ea6808955605c03ac312d835c8
AlanDara <59530864+AlanDara@users.noreply.github.com>
2020-09-23 14:54:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${client.users.cache.size} users, ${client.guilds.cache.size} guilds, ${PREFIX}help`);
+  client.user.setActivity(`%help ,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}, Guilds ${client.guilds.cache.size}`);
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
