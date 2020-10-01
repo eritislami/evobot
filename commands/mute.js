fcf4490559c0c0c959571a2a8666e70567da3ca4
L0SER <65214509+L0SER8228@users.noreply.github.com>
2020-10-01 08:22:09 +0530

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,19 @@
+const { canModifyQueue } = require("../util/EvobotUtil");
+
+
+module.exports = {
+  name: "mute",
+  aliases: ["silent"],
+  description: "Silent the music volume",
+  execute(message) {
+    const queue = message.client.queue.get(message.guild.id);
+    
+    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!canModifyQueue(message.member)) return;
+
+    global.volumeold = queue.volume;
+    queue.volume = 0;
+    queue.connection.dispatcher.setVolumeLogarithmic(0);
+    queue.textChannel.send(`${user} 🔇 muted the music!`).catch(console.error);
+  }
+};
