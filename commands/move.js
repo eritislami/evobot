2c7da87ac7f41f52ce453a6ad5bb108b67321c0f
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-10-06 15:55:39 -0500

    add move command

---
+++ commands/move.js
@@ -0,0 +1,22 @@
+require('array.prototype.move');
+const { canModifyQueue } = require("../util/EvobotUtil");
+
+module.exports = {
+  name: "move",
+  aliases: ["mv"],
+  description: "Move songs to the top of the queue",
+  execute(message, args) {
+    const queue = message.client.queue.get(message.guild.id);
+    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    if (!canModifyQueue(message.member)) return;
+    
+    if (!args.length) return message.reply(`Usage: ${message.client.prefix}move <Queue Number>`);
+    if (isNaN(args[0])) return message.reply(`Usage: ${message.client.prefix}move <Queue Number>`);
+
+    
+    console.log(`songs: ${queue.songs}`);
+
+    const song = queue.songs.move(args[0] - 1, 1);
+    queue.textChannel.send(`${message.author} 🚚 moved **${song[args[0]].title}** to the top of the queue.`);
+  }
+};
