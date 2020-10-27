38d4a6cdbec2024d7b1a09cb6dc86c068b08070e
Erit Islami <eritislami@gmail.com>
2020-10-27 21:10:36 +0100

    fix: add max time to collector

--- commands/queue.js
+++ commands/queue.js
@@ -20,7 +20,7 @@ module.exports = {
 
       const filter = (reaction, user) =>
         ["⬅️", "⏹", "➡️"].includes(reaction.emoji.name) && message.author.id === user.id;
-      const collector = queueEmbed.createReactionCollector(filter);
+      const collector = queueEmbed.createReactionCollector(filter, { time: 60000 });
 
       collector.on("collect", async (reaction, user) => {
         try {
