5aa2646139741a43e260b409748942261b5c3e25
Mu7aMMaD <69051329+Muhammad-Hadidi@users.noreply.github.com>
2020-10-27 17:55:29 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,27 +1,36 @@
-const { MessageEmbed } = require("discord.js");
+const { prefix } = require("../config/config.json");
 
 module.exports = {
   name: "help",
-  aliases: ["h"],
-  description: "Display all commands and descriptions",
-  execute(message) {
-    let commands = message.client.commands.array();
+  description: "List all of my commands or info about a specific command.",
+  aliases: "commands",
+  usage: "[command name]",
+  execute(message, args) {
+    const data = [];
+    const { commands } = message.client;
 
-    let helpEmbed = new MessageEmbed()
-      .setTitle("Evobot Help")
-      .setDescription("List of all commands")
-      .setColor("#F8AA2A");
+    if (!args.length) {
+      data.push("Here's a list of all my commands:");
+      data.push(commands.map((command) => command.name).join(", "));
+      data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-    commands.forEach((cmd) => {
-      helpEmbed.addField(
-        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
-        `${cmd.description}`,
-        true
-      );
-    });
+      return message.channel.send(data, { split: true });
+    }
 
-    helpEmbed.setTimestamp();
+    const name = args[0].toLowerCase();
+    const command =
+      commands.get(name) ||
+      commands.find((c) => c.aliases && c.aliases.includes(name));
 
-    return message.channel.send(helpEmbed).catch(console.error);
-  }
+    if (!command) { return message.reply("that's not a valid command!");}
+
+    data.push(`❯ **Name:** ${command.name}`);
+    data.push(`❯ **Description:** ${command.description || "No Description provided."}`);
+    data.push(`❯ **Aliases:** ${command.aliases ? command.aliases.join(", ") : "None"}`);
+    data.push(`❯ **Usage:** ${command.usage? `\`${prefix}${command.name} ${command.usage}\``: "No Usage"}`);
+    data.push(`❯ **Cooldown:** ${command.cooldown ? `\`${command.cooldown} Second's\`` : "No cooldown."}`
+    );
+
+    message.channel.send(data, { split: true });
+  },
 };
