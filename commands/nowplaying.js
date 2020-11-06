c385ef53988d750c72eb545dd9089f917daa1a58
Erit Islami <eritislami@gmail.com>
2020-11-06 20:49:17 +0100

    fix(nowplaying): don't show progress if duration is missing from song

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -7,6 +7,7 @@ module.exports = {
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+
     const song = queue.songs[0];
     const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
     const left = song.duration - seek;
@@ -15,8 +16,10 @@ module.exports = {
       .setTitle("Now playing")
       .setDescription(`${song.title}\n${song.url}`)
       .setColor("#F8AA2A")
-      .setAuthor("EvoBot")
-      .addField(
+      .setAuthor("EvoBot");
+
+    if (song.duration > 0) {
+      nowPlaying.addField(
         "\u200b",
         new Date(seek * 1000).toISOString().substr(11, 8) +
           "[" +
@@ -25,9 +28,8 @@ module.exports = {
           (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000).toISOString().substr(11, 8)),
         false
       );
-
-    if (song.duration > 0)
       nowPlaying.setFooter("Time Remaining: " + new Date(left * 1000).toISOString().substr(11, 8));
+    }
 
     return message.channel.send(nowPlaying);
   }
