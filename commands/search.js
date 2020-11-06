ee9af7b94c39f8f4669b87d0a587e6fdbaf15243
Erit Islami <eritislami@gmail.com>
2020-11-06 20:45:44 +0100

    feat(search): add ability to select multiple songs from search

--- commands/search.js
+++ commands/search.js
@@ -15,7 +15,9 @@ module.exports = {
   description: "Search and select videos to play",
   async execute(message, args) {
     if (!args.length)
-      return message.reply(`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`).catch(console.error);
+      return message
+        .reply(`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`)
+        .catch(console.error);
     if (message.channel.activeCollector)
       return message.reply("A message collector is already active in this channel.");
     if (!message.member.voice.channel)
@@ -32,23 +34,37 @@ module.exports = {
       const results = await youtube.searchVideos(search, 10);
       results.map((video, index) => resultsEmbed.addField(video.shortURL, `${index + 1}. ${video.title}`));
 
-      var resultsMessage = await message.channel.send(resultsEmbed);
+      let resultsMessage = await message.channel.send(resultsEmbed);
 
       function filter(msg) {
-        const pattern = /(^[1-9][0-9]{0,1}$)/g;
-        return pattern.test(msg.content) && parseInt(msg.content.match(pattern)[0]) <= 10;
+        const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/g;
+        return pattern.test(msg.content);
       }
 
       message.channel.activeCollector = true;
       const response = await message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] });
-      const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
+      const reply = response.first().content;
+
+      if (reply.includes(",")) {
+        let songs = reply.split(",").map((str) => str.trim());
+
+        for (let song of songs) {
+          await message.client.commands
+            .get("play")
+            .execute(message, [resultsEmbed.fields[parseInt(song) - 1].name]);
+        }
+      } else {
+        const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
+        message.client.commands.get("play").execute(message, [choice]);
+      }
 
       message.channel.activeCollector = false;
-      message.client.commands.get("play").execute(message, [choice]);
       resultsMessage.delete().catch(console.error);
+      response.first().delete().catch(console.error);
     } catch (error) {
       console.error(error);
       message.channel.activeCollector = false;
+      message.reply(error.message).catch(console.error);
     }
   }
 };
