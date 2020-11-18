b943ca4e7d477c95638ce2381bdccc2656cb0966
JediThePro <pasit.sakorn@gmail.com>
2020-11-18 12:29:33 +0700

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -5,13 +5,27 @@ module.exports = {
   description: "Pause the currently playing music",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    
+    const emptyQueue = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setTitle('Empty Queue')
+    .setDescription('There is nothing playing')
+    
+    if (!queue) return message.reply(emptyQueue).catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (queue.playing) {
       queue.playing = false;
       queue.connection.dispatcher.pause(true);
-      return queue.textChannel.send(`${message.author} ⏸ paused the music.`).catch(console.error);
+      
+      const paused = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setTitle('Paused')
+    .setDescription(`${message.author} ⏸ paused the music`)
+     
+      return queue.textChannel.send(paused).catch(console.error);
     }
   }
 };
