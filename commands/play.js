619b923d93bc78a65beaf8c9507278c83dbff832
JediThePro <pasit.sakorn@gmail.com>
2020-11-18 13:01:46 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -2,6 +2,7 @@ const { play } = require("../include/play");
 const ytdl = require("ytdl-core");
 const YouTubeAPI = require("simple-youtube-api");
 const scdl = require("soundcloud-downloader");
+const { MessageEmbed } = require('discord.js');
 
 let YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID;
 try {
@@ -23,9 +24,24 @@ module.exports = {
     const { channel } = message.member.voice;
 
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!channel) return message.reply("You need to join a voice channel first!").catch(console.error);
+    
+    const requiredVC = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setAuthor(`${message.author.tag}`)
+    .setTitle('Error!')
+    .setDescription('Please join a voice channel before run this command')
+    
+    if (!channel) return message.channel.send(requiredVC).catch(console.error);
     if (serverQueue && channel !== message.guild.me.voice.channel)
-      return message.reply(`You must be in the same channel as ${message.client.user}`).catch(console.error);
+      const sameVC = new MessageEmbed()
+      .setColor(0xda7272)
+      .setTimestamp()
+      .setAuthor(`${message.author.tag}`)
+      .setTitle('Error!')
+      .setDescription(`You must be in the same channel as ${message.client.user}`)
+      
+      return message.channel.send(sameVC).catch(console.error);
 
     if (!args.length)
       return message
@@ -34,9 +50,23 @@ module.exports = {
 
     const permissions = channel.permissionsFor(message.client.user);
     if (!permissions.has("CONNECT"))
-      return message.reply("Cannot connect to voice channel, missing permissions");
+      
+      const vcError = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setTitle('Voice Channel Error!')
+    .setDescription('Cannot connect to the voice channel, Missing Permissions')
+      
+      return message.reply(vcError);
     if (!permissions.has("SPEAK"))
-      return message.reply("I cannot speak in this voice channel, make sure I have the proper permissions!");
+      const unableSpeak = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setAuthor(`${message.author.tag}`)
+    .setTitle('Audio Error!')
+    .setDescription('I cannot speak in this voice channel, make sure I have the proper permissions')
+      
+      return message.channel.send(unableSpeak);
 
     const search = args.join(" ");
     const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
@@ -107,7 +137,14 @@ module.exports = {
     if (serverQueue) {
       serverQueue.songs.push(song);
       return serverQueue.textChannel
-        .send(`✅ **${song.title}** has been added to the queue by ${message.author}`)
+        const addedSongToQueue = new MessageEmbed()
+    .setColor(0x7289da)
+    .setTimestamp()
+        .setAuthor(`**${song.title}** has been added to the queue by ${message.author.tag}`)
+    .setTitle('Song Added To Queue')
+    .setDescription(`Song added by ${message.author.tag}`)
+      
+        .send(addedSongToQueue)
         .catch(console.error);
     }
 
@@ -122,7 +159,14 @@ module.exports = {
       console.error(error);
       message.client.queue.delete(message.guild.id);
       await channel.leave();
-      return message.channel.send(`Could not join the channel: ${error}`).catch(console.error);
+      
+      const unableJoin = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setTitle('Error!')
+    .setDescription(`Could not join join the channel: ${error}`)
+      
+      return message.channel.send(unableJoin).catch(console.error);
     }
   }
 };
