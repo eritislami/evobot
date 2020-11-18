525e444646a80645380ae2bace8b0075dcb9ecac
Denis Mignanelli <denistschurtschenthaler@gmail.com>
2020-11-18 15:42:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help`);
+  client.user.setActivity(`KKQQ`);
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
