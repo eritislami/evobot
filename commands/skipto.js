7cf14a1f2526e8da177495299ca079eb89db6c7a
JediThePro <pasit.sakorn@gmail.com>
2020-11-18 20:26:38 +0700

    Update skipto.js

--- commands/skipto.js
+++ commands/skipto.js
@@ -1,20 +1,40 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: "skipto",
   aliases: ["st"],
   description: "Skip to the selected queue number",
   execute(message, args) {
+    const noQa = new MessageEmbed()
+    .setColor(0xd3d3d3)
+    .setTimestamp()
+    .setTitle('Usage')
+    .setDescription(`${message.client.prefix}${module.exports.name} <Queue Number>`)
+    
     if (!args.length || isNaN(args[0]))
       return message
-        .reply(`Usage: ${message.client.prefix}${module.exports.name} <Queue Number>`)
+        .reply(noQa)
         .catch(console.error);
 
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    
+    const noQ = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setTitle('Empty Queue')
+    .setDescription('There is nothing in the queue')
+    
+    if (!queue) return message.channel.send(noQ).catch(console.error);
     if (!canModifyQueue(message.member)) return;
     if (args[0] > queue.songs.length)
-      return message.reply(`The queue is only ${queue.songs.length} songs long!`).catch(console.error);
+      const noQw = new MessageEmbed()
+    .setColor(0xda7272)
+    .setTimestamp()
+    .setTitle('Error!')
+    .setDescription(`The queue is only ${queue.songs.length} songs long!`)
+      
+      return message.reply(noQw).catch(console.error);
 
     queue.playing = true;
 
@@ -27,6 +47,13 @@ module.exports = {
     }
 
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏭ skipped ${args[0] - 1} songs`).catch(console.error);
+    
+    const skipto = new MessageEmbed()
+    .setColor(0x7289da)
+    .setTimestamp()
+    .setTitle('Skipped!')
+    .setDescription(`${message.author.tag} skipped ${args[0] - 1} songs`)
+    
+    queue.textChannel.send(skipto).catch(console.error);
   }
 };
