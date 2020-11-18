0b9da9204fecf95f53f156b3feab8899efa44e9f
JediThePro <pasit.sakorn@gmail.com>
2020-11-18 18:42:30 +0700

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -1,4 +1,5 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: "resume",
@@ -6,15 +7,37 @@ module.exports = {
   description: "Resume currently playing music",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    
+    
+      const nothingPlaying = new MessageEmbed()
+      .setColor(0xda7272)
+      .setTimestamp()
+      .setTitle('Error!')
+      .setDescription(`There is nothing playing`)
+    
+    if (!queue) return message.reply(nothingPlaying).catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (!queue.playing) {
       queue.playing = true;
       queue.connection.dispatcher.resume();
-      return queue.textChannel.send(`${message.author} ▶ resumed the music!`).catch(console.error);
+      
+      
+      const resumed = new MessageEmbed()
+      .setColor(0x7289da)
+      .setTimestamp()
+      .setTitle('Resumed')
+      .setDescription(`${message.author} ▶ resumed the music`)
+      
+      return queue.textChannel.send(resumed).catch(console.error);
     }
-
-    return message.reply("The queue is not paused.").catch(console.error);
+    const notPaused = new MessageEmbed()
+      .setColor(0xda7272)
+      .setTimestamp()
+      .setTitle('Error!')
+      .setDescription(`The song/queue is not paused`)
+    
+    
+    return message.reply(notPaused).catch(console.error);
   }
 };
