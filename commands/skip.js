c7d238ecad25fa42935a7413ac6c5e9dea82ab50
JediThePro <pasit.sakorn@gmail.com>
2020-11-18 18:48:00 +0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,4 +1,5 @@
 const { canModifyQueue } = require("../util/EvobotUtil");
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: "skip",
@@ -7,11 +8,24 @@ module.exports = {
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue)
-      return message.reply("There is nothing playing that I could skip for you.").catch(console.error);
+      const noQ2Skip = new MessageEmbed()
+      .setColor(0xda7272)
+      .setTimestamp()
+      .setTitle('Unable to Skip')
+      .setDescription(`There is nothing playing that I could skip for you`)
+      
+      return message.reply(noQ2Skip).catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.playing = true;
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏭ skipped the song`).catch(console.error);
+    
+    const skipped = new MessageEmbed()
+      .setColor(0x7289da)
+      .setTimestamp()
+      .setTitle('Skipped')
+      .setDescription(`${message.author.tag} skipped the song`)
+    
+    queue.textChannel.send(skipped).catch(console.error);
   }
 };
