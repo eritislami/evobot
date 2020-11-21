00d23b14309559bb6d4d2522179d21f59fcb6daf
Pyxis <pyxis@uzuki.live>
2020-11-22 05:17:58 +0900

    add translation for leaving channel (related to #351)

--- include/play.js
+++ include/play.js
@@ -14,7 +14,7 @@ module.exports = {
       setTimeout(function () {
         if (queue.connection.dispatcher && message.guild.me.voice.channel) return;
         queue.channel.leave();
-        queue.textChannel.send("Leaving voice channel...");
+        queue.textChannel.send(i18n.__("play.leaveChannel"));
       }, STAY_TIME * 1000);
       return queue.textChannel.send(i18n.__("play.queueEnded")).catch(console.error);
       return message.client.queue.delete(message.guild.id);
