533719f6e2b6c270a77bc04acc950df314f86528
evrardco <colin.evrard@student.uclouvain.be>
2020-12-02 23:14:35 +0100

    Feature: Default volume. (#417)
    * added a default volume feature
    * fix: set to 100 if var is missing from config
    
    Co-authored-by: Erit Islami <eritislami@gmail.com>

--- commands/play.js
+++ commands/play.js
@@ -2,7 +2,7 @@ const { play } = require("../include/play");
 const ytdl = require("ytdl-core");
 const YouTubeAPI = require("simple-youtube-api");
 const scdl = require("soundcloud-downloader");
-const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID } = require("../util/EvobotUtil");
+const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID, DEFAULT_VOLUME } = require("../util/EvobotUtil");
 const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
 
 module.exports = {
@@ -49,7 +49,7 @@ module.exports = {
       connection: null,
       songs: [],
       loop: false,
-      volume: 100,
+      volume: DEFAULT_VOLUME || 100,
       playing: true
     };
 
