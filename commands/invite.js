aaade0c7e2357f92a6f17f23571dfe236608a856
Pyxis <pyxis@uzuki.live>
2020-12-02 21:22:55 +0900

    add translation for clip, clips, invite

--- commands/invite.js
+++ commands/invite.js
@@ -1,6 +1,11 @@
+const { LOCALE } = require("../util/EvobotUtil");
+const i18n = require("i18n");
+
+i18n.setLocale(LOCALE);
+
 module.exports = {
   name: "invite",
-  description: "Send bot invite link",
+  description: i18n.__('invite.description'),
   execute(message) {
     return message.member
       .send(
