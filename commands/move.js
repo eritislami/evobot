a5b5cb305a21a2e8a9ea748b0a4688ffee6f4e2b
Erit Islami <eritislami@gmail.com>
2020-12-05 01:56:00 +0100

    feat: use array-move instead of prototype and allow moving everywhere

--- commands/move.js
+++ commands/move.js
@@ -1,21 +1,23 @@
-require('array.prototype.move');
+const move = require("array-move");
 const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "move",
   aliases: ["mv"],
-  description: "Move songs to the top of the queue",
+  description: "Move songs around in the queue",
   execute(message, args) {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) return message.channel.send("There is no queue.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
-    
+
     if (!args.length) return message.reply(`Usage: ${message.client.prefix}move <Queue Number>`);
-    if (isNaN(args[0])) return message.reply(`Usage: ${message.client.prefix}move <Queue Number>`);
+    if (isNaN(args[0]) || args[0] <= 1) return message.reply(`Usage: ${message.client.prefix}move <Queue Number>`);
 
-    let songMoved = queue.songs[args[0] - 1];
+    let song = queue.songs[args[0] - 1];
 
-    queue.songs.move(args[0] - 1, 1);
-    queue.textChannel.send(`${message.author} 🚚 moved **${songMoved.title}** to the top of the queue.`);
+    queue.songs = move(queue.songs, args[0] - 1, args[1] == 1 ? 1 : args[1] - 1);
+    queue.textChannel.send(
+      `${message.author} 🚚 moved **${song.title}** to ${args[1] == 1 ? 1 : args[1] - 1} in the queue.`
+    );
   }
 };
