888658435c4e03a55327fda87fa7b53e257fb749
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2020-12-24 01:09:26 -0800

    Remove proxy, use HTTPS

--- webclient/src/QueueService.js
+++ webclient/src/QueueService.js
@@ -1,4 +1,4 @@
 export async function getQueue() {
     console.debug(`${new Date().toString()}: Fetching queue`);
-    return await fetch("/api/queue");
+    return await fetch("https://fred.skydev.one:8080/api/queue");
 }
