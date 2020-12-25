c9017ab0fd63535ef7aecdcae5879f429f0d468a
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2020-12-24 23:53:44 -0800

    Update Dockerfile

--- webclient/src/QueueService.js
+++ webclient/src/QueueService.js
@@ -1,4 +1,4 @@
 export async function getQueue() {
     console.debug(`${new Date().toString()}: Fetching queue`);
-    return await fetch("https://fred.skydev.one:8080/api/queue");
+    return await fetch("https://fred.skydev.one:8081/api/queue");
 }
