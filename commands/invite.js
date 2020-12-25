37959c9695c5afd6f9eb0f8bd85575634a886293
ItsNotkungZ <69802296+ItsNotkungZ@users.noreply.github.com>
2020-12-26 04:12:17 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "invite",
   description: "Send bot invite link",
-  execute(message) {
+  execute(client, message) {
     return message.member
       .send(
         `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=70282305&scope=bot
