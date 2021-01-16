da26cf3c9077a9af441679bca000155604f39c8e
Techroy23 <53241974+techroy23@users.noreply.github.com>
2021-01-16 12:52:55 +0800

    Update update.sh
    added $PWD on nohup command

--- update.sh
+++ update.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
 # This script will kill evobot process and pull the latest git.
 # make sure that your files are save in the evobot folder.
 
+echo searching PID
 VARPID="$(ps aux | grep evobot | head -1 | awk '{print $2}')"
 
 echo FOUND : $VARPID
+echo KILLING $VARPID
 kill $VARPID
 
+echo PULLING evobot
 git pull https://github.com/eritislami/evobot
 
+echo UPDATING NPM
 npm install
 npm update
 
-nohup node index.js &
+echo STARTING
+nohup node $PWD/index.js &
 
 exit 0
