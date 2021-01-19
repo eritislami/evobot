c2efda0ba0eb2fe0298f485576f1c12ee30dbf7c
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2021-01-18 20:54:48 -0800

    Save played songs to db

--- firebase.js
+++ firebase.js
@@ -1,22 +1,72 @@
 var admin = require("firebase-admin");
+const { user } = require("firebase-functions/lib/providers/auth");
 
-var serviceAccount = require("./rythmaddon-firebase-adminsdk-l059e-fd6702435f.json");
+const { FIREBASE_ACCOUNT } = require("./util/EvobotUtil");
+const songHistoryDb = "song_history";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+let db;
+
+if (FIREBASE_ACCOUNT) {
+  admin.initializeApp({
+    credential: admin.credential.cert(FIREBASE_ACCOUNT)
+  });
+  db = admin.firestore();
+} else {
+  console.warn("No firebase account specified. Data will not be recorded in the database.")
+}
 
 module.exports = {
   async getAllSongHistory() {
-    const docRef = admin.firestore().collection("song_history").doc("o78wkOea9TnK5DSylo29");
-    const doc = await docRef.get();
-    if (!doc.exists) {
-      console.log("Document not found");
-    } else {
-      const data = doc.data();
-      console.log(`Data: ${JSON.stringify(data)}`)
-    }
+    if (!FIREBASE_ACCOUNT) return;
+    const docRef = db.collection(songHistoryDb).get();
+    docRef.forEach((doc) => {
+      console.log(doc.data());
+    });
 
-    return doc;
+    return docRef;
+  },
+
+  // TODO: make it work async
+  saveSong(song) {
+    if (!FIREBASE_ACCOUNT) return;
+    const songJson = JSON.parse(JSON.stringify(song));
+    console.info(song);
+    const searchParams = new URLSearchParams(new URL(song.url).search);
+    const songId = searchParams.get('v');
+    console.info(`Video ID: ${songId}`);
+    const doc = db.collection(songHistoryDb).doc(songId);
+    const timestamp = Date.now();
+    
+    doc.get().then(docData => {
+      if (docData.exists) {
+        console.info(`${songId} already exists: ${doc.data()}`);
+        songJson = JSON.parse(doc.data);
+        songJson.user.append({
+          id: song.user.id,
+          username: song.user.username,
+          time: timestamp
+        });
+        songJson.totalPlays += 1;
+        songJson.lastPlay = timestamp;
+        doc.set(songJson);
+        }
+    });
+    if (doc.data) {
+      
+    } else {
+      console.info(`${songId} not found`);
+      try {
+        songJson.user = [{
+          id: song.user.id,
+          username: song.user.username,
+          time: timestamp
+        }];
+        songJson.totalPlays =  1;
+        songJson.lastPlay = timestamp;
+        doc.set(songJson);
+      } catch(err) {
+        console.error(err);
+      }
+    }
   }
 }
