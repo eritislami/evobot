6b4326cc5ac0c6c978e0c6fa497d30fe00078fff
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-01 17:37:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
+  client.user.setActivity(`tunes. Type:${PREFIX}help for more info`, { type: "STREAMING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
