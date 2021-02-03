c4be6455faad3c2dd9e855171cd44d29a8b672b3
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2021-02-02 21:50:25 -0800

    Start adding css, separate header
    (cherry picked from commit a217318ee03ac8c5de0b005944c31672b047927a)

--- webclient-vue/src/App.vue
+++ webclient-vue/src/App.vue
@@ -1,40 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <a v-if="!is_logged_in" :href="discord_authorize_url">Login</a>
-      <a v-if="is_logged_in" href="/profile">{{discord_user.username}}'s Profile</a>
-      <a v-if="is_logged_in" @click="logout()" href="/">Logout</a>
-    </div>
+    <NavHeader/>
+    <h1>Main</h1>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import {logout, login} from './auth/discord'
-import {discord} from '../auth_config'
+import {login} from './auth/discord'
+import NavHeader from './components/NavHeader'
 export default {
   name: 'App',
   components: {
-    
-  },
-  methods: {
-    logout: function () {
-      logout()
-    }
-  },
-  computed: {
-    is_logged_in: function() {
-      let user = this.$store.state.user
-      return Boolean(user)
-    },
-    discord_user: function() {
-      let user = JSON.parse(localStorage.getItem('discord_user'))
-      return user
-    },
-    discord_authorize_url: function() {
-      return `https://discord.com/api/oauth2/authorize?client_id=${discord.client_id}&redirect_uri=${encodeURIComponent(discord.redirect_uri)}&response_type=code&scope=${discord.scope}`
-    }
+    NavHeader
   },
   mounted() {
     const fragment = new URLSearchParams(window.location.search)
@@ -55,5 +33,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+body {
+  background: #1A1E23;
 }
 </style>
