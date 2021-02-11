df2d9ad93fae230b3ab5d27e31c9d2ea0cf6f16a
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-11 18:01:48 -0500

    Update shipmod.js

--- shipmod.js
+++ shipmod.js
@@ -137,3 +137,13 @@ let evobot;
   
   
 })();
+
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(process.env.PORT||80);
