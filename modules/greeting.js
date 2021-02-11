83675b4ac3b5dd3eeb1a4e07c92b5ef7e2d411dd
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-11 18:11:27 -0500

    Update greeting.js

--- modules/greeting.js
+++ modules/greeting.js
@@ -1,15 +1,15 @@
-const i18n = require("i18n");
+// const i18n = require("i18n");
 
-  const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
+//   const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+//   const commandName = args.shift().toLowerCase();
 
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply(i18n.__("common.errorCommend")).catch(console.error);
-  }
+//   try {
+//     command.execute(message, args);
+//   } catch (error) {
+//     console.error(error);
+//     message.reply(i18n.__("common.errorCommend")).catch(console.error);
+//   }
 
 
 
