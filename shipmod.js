b9c066808a57ffde8ce5db230b1bee57b40cc4cc
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-02-11 19:27:11 -0500

    Update shipmod.js

--- shipmod.js
+++ shipmod.js
@@ -193,7 +193,7 @@ let evobot;
     lastKeepAlive=Date.now();
   };
 
-
+  const http = require('http');
   const requestListener = function (req, res) {
     res.writeHead(200);
     res.end('Hello, World!');
