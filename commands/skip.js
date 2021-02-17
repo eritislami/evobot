d6744f001bfceef18230c610fbabc93ce92af5c9
L0SER <65214509+L0SER8228@users.noreply.github.com>
2021-02-17 19:42:46 +0530

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -12,6 +12,9 @@ module.exports = {
     if (!queue) return message.reply(i18n.__("skip.errorNotQueue")).catch(console.error);
     if (!canModifyQueue(message.member)) return i18n.__("common.errorNotChannel");
 
+    //turn off loopSong
+    queue.loopSong = false;
+
     queue.playing = true;
     queue.connection.dispatcher.end();
     queue.textChannel.send(i18n.__mf("skip.result", { author: message.author })).catch(console.error);
