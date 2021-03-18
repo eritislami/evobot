650114408d182a973ca0d93c291fea031db4579c
miracl-cutelemon173 <76854136+miracl-cutelemon173@users.noreply.github.com>
2021-03-18 10:09:10 +0800

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,65 +1,44 @@
 const { MessageEmbed } = require("discord.js");
+const { YOUTUBE_API_KEY } = require("../config.json");
 const YouTubeAPI = require("simple-youtube-api");
-const { YOUTUBE_API_KEY, LOCALE } = require("../util/EvobotUtil");
 const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
-const i18n = require("i18n");
-
-i18n.setLocale(LOCALE);
 
 module.exports = {
-  name: "search",
-  description: i18n.__("search.description"),
+  name: "youube",
   async execute(message, args) {
     if (!args.length)
-      return message
-        .reply(i18n.__mf("search.usageReply", { prefix: message.client.prefix, name: module.exports.name }))
-        .catch(console.error);
-    if (message.channel.activeCollector) return message.reply(i18n.__("search.errorAlreadyCollector"));
-    if (!message.member.voice.channel)
-      return message.reply(i18n.__("search.errorNotChannel")).catch(console.error);
+      return message.reply(`格式錯誤 正確格式: ${message.client.prefix}${module.exports.name} <影片名稱>`).catch(console.error);
+    if (message.channel.activeCollector)
+      return message.reply("A message collector is already active in this channel.");
 
     const search = args.join(" ");
 
     let resultsEmbed = new MessageEmbed()
-      .setTitle(i18n.__("search.resultEmbedTtile"))
-      .setDescription(i18n.__mf("search.resultEmbedDesc", { search: search }))
+      .setTitle(`**YouTube搜索！**`)
+      .setDescription(`${search} 搜索結果`)
       .setColor("#F8AA2A");
 
     try {
       const results = await youtube.searchVideos(search, 10);
       results.map((video, index) => resultsEmbed.addField(video.shortURL, `${index + 1}. ${video.title}`));
 
-      let resultsMessage = await message.channel.send(resultsEmbed);
+      var resultsMessage = await message.channel.send(resultsEmbed);
 
       function filter(msg) {
-        const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/;
-        return pattern.test(msg.content);
+        const pattern = /(^[1-9][0-9]{0,1}$)/g;
+        return pattern.test(msg.content) && parseInt(msg.content.match(pattern)[0]) <= 10;
       }
 
       message.channel.activeCollector = true;
       const response = await message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] });
-      const reply = response.first().content;
-
-      if (reply.includes(",")) {
-        let songs = reply.split(",").map((str) => str.trim());
-
-        for (let song of songs) {
-          await message.client.commands
-            .get("play")
-            .execute(message, [resultsEmbed.fields[parseInt(song) - 1].name]);
-        }
-      } else {
-        const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
-        message.client.commands.get("play").execute(message, [choice]);
-      }
+      const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
 
       message.channel.activeCollector = false;
+      message.client.commands.get("play").execute(message, [choice]);
       resultsMessage.delete().catch(console.error);
-      response.first().delete().catch(console.error);
     } catch (error) {
       console.error(error);
       message.channel.activeCollector = false;
-      message.reply(error.message).catch(console.error);
     }
   }
 };
