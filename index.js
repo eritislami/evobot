1518c33009828e8d9f1f662691f7101fbb6647a6
Erit Islami <eritislami@gmail.com>
2021-04-18 18:32:58 +0200

    fix(locales): update list

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const cooldowns = new Collection();
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 i18n.configure({
-  locales: ["en", "es", "ko", "fr", "tr", "pt_br", "zh_cn", "zh_tw", "ru"],
+  locales: ["de", "en", "es", "fr", "it", "ko", "pt_br", "ru", "tr", "zh_cn", "zh_tw"],
   directory: path.join(__dirname, "locales"),
   defaultLocale: "en",
   objectNotation: true,
