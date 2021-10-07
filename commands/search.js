50a238c95102e56fd7f382f6c4c3ac2b9618fbae
Drew Brown <drewburr9@gmail.com>
2021-10-06 22:30:19 -0500

    Update regex to only accept numbers 1-10 (#981)

--- commands/search.js
+++ commands/search.js
@@ -30,7 +30,7 @@ module.exports = {
       let resultsMessage = await message.channel.send(resultsEmbed);
 
       function filter(msg) {
-        const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/;
+        const pattern = /^[1-9][0]?(\s*,\s*[1-9][0]?)*$/;
         return pattern.test(msg.content);
       }
 
