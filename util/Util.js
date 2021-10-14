ee2968ab5323c4a6c284913a0084d5d094f5ec68
Shiro-Nek0 <andres.barra04@gmail.com>
2021-10-14 09:58:04 -0300

    modified:   util/Util.js

--- util/Util.js
+++ util/Util.js
@@ -9,7 +9,6 @@ try {
 }
 
 exports.TOKEN = config ? config.TOKEN : process.env.TOKEN;
-exports.YOUTUBE_API_KEY = config ? config.YOUTUBE_API_KEY : process.env.YOUTUBE_API_KEY;
 exports.SOUNDCLOUD_CLIENT_ID = config ? config.SOUNDCLOUD_CLIENT_ID : process.env.SOUNDCLOUD_CLIENT_ID;
 exports.PREFIX = (config ? config.PREFIX : process.env.PREFIX) || "/";
 exports.MAX_PLAYLIST_SIZE = (config ? config.MAX_PLAYLIST_SIZE : parseInt(process.env.MAX_PLAYLIST_SIZE)) || 10;
