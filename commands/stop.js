a942eeb79c5fd828a811e83e9c06384dde47d8ae
Swarup <swarupdaz@gmail.com>
2021-10-29 03:24:58 +0530

    added aliases to stop command

--- commands/stop.js
+++ commands/stop.js
@@ -3,6 +3,8 @@ const i18n = require("../util/i18n");
 
 module.exports = {
   name: "stop",
+  aliases: ["fuckoff"],
+  aliases: ["unsummon"],
   description: i18n.__("stop.description"),
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
