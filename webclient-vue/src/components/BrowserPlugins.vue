2d3e8e82d6089248cc85bb1d5163a6a1df21ffa7
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2022-01-16 20:48:54 -0800

    Add browser addon links in footer

---
+++ webclient-vue/src/components/BrowserPlugins.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <table style='margin-left:auto; margin-right:auto'>
+            <tr>
+                <td>
+                    <a href='https://chrome.google.com/webstore/detail/hgpmbjkiigbpcofedhglalcpmgikmeib'>Chrome Addon</a>
+                </td>
+                <td>
+                    <a href='https://fbe-authserver-uaxpuwzs5q-uw.a.run.app/static/ffaddon/fredbot-0.0.2-fx.xpi'>Firefox Addon</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BrowserPlugins'
+}
+</script>
+
+
+<style>
+
+</style>
--- webclient-vue/src/components/Home.vue
+++ webclient-vue/src/components/Home.vue
@@ -1,16 +1,18 @@
 <template>
 <div>
     <SongTable/>
+    <BrowserPlugins/>
 </div>
 </template>
 
 <script>
 import SongTable from './SongTable'
+import BrowserPlugins from './BrowserPlugins.vue'
 
 export default {
     name: 'Home',
     components: {
-        SongTable
+        SongTable, BrowserPlugins
     }
 }
 </script>
