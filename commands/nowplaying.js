6e440b245ec3739b37b3a96e65fd59911ab2dc58
Erit Islami <eritislami@gmail.com>
2022-04-25 23:29:53 +0200

    fix(np): refactor to esm and restore seekbar

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,26 +1,27 @@
-const { splitBar } = require("string-progressbar");
-const { MessageEmbed } = require("discord.js");
-const i18n = require("../util/i18n");
+import { MessageEmbed } from "discord.js";
+import { splitBar } from "string-progressbar";
+import { i18n } from "../utils/i18n.js";
 
-module.exports = {
+export default {
   name: "np",
+  cooldown: 10,
   description: i18n.__("nowplaying.description"),
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
+
     if (!queue || !queue.songs.length)
       return message.reply(i18n.__("nowplaying.errorNotQueue")).catch(console.error);
 
     const song = queue.songs[0];
-    const seek = queue.resource.playbackDuration;
+    const seek = queue.resource.playbackDuration / 1000;
     const left = song.duration - seek;
 
     let nowPlaying = new MessageEmbed()
       .setTitle(i18n.__("nowplaying.embedTitle"))
       .setDescription(`${song.title}\n${song.url}`)
-      .setColor("#F8AA2A")
-      .setAuthor({ name: message.client.user.username });
+      .setColor("#F8AA2A");
 
-    /* if (song.duration > 0) {
+    if (song.duration > 0) {
       nowPlaying.addField(
         "\u200b",
         new Date(seek * 1000).toISOString().substr(11, 8) +
@@ -30,11 +31,14 @@ module.exports = {
           (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000).toISOString().substr(11, 8)),
         false
       );
-      nowPlaying.setFooter(
-        i18n.__mf("nowplaying.timeRemaining", { time: new Date(left * 1000).toISOString().substr(11, 8) })
-      );
-    } */
 
-    return message.channel.send({ embeds: [nowPlaying] });
+      nowPlaying.setFooter({
+        text: i18n.__mf("nowplaying.timeRemaining", {
+          time: new Date(left * 1000).toISOString().substr(11, 8)
+        })
+      });
+    }
+
+    return message.reply({ embeds: [nowPlaying] });
   }
 };
