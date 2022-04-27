641b1cc6d62df382387c8204ccee62954ec3523d
Erit Islami <eritislami@gmail.com>
2022-04-28 00:48:59 +0200

    fix(patterns): export for reusability

---
+++ utils/patterns.js
@@ -0,0 +1,4 @@
+export const videoPattern = /^(https?:\/\/)?(www\.)?(m\.|music\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+export const playlistPattern = /^.*(list=)([^#\&\?]*).*/gi;
+export const scRegex = /^https?:\/\/(soundcloud\.com)\/(.*)$/;
+export const mobileScRegex = /^https?:\/\/(soundcloud\.app\.goo\.gl)\/(.*)$/;
