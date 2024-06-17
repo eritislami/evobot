4bb7c0f8a7856067f6c947da9f5b14d4863b9645
crackodille <crackodille@gmail.com>
2024-06-18 00:15:21 +0200

    fix(song.ts): changing youtube-sr to ytdl-core in Song.ts makeResource()
    fix #1644

--- structs/Song.ts
+++ structs/Song.ts
@@ -4,6 +4,7 @@ import { i18n } from "../utils/i18n";
 import { videoPattern, isURL } from "../utils/patterns";
 
 const { stream, video_basic_info } = require("play-dl");
+import ytdl from "ytdl-core";
 
 export interface SongData {
   url: string;
@@ -60,18 +61,20 @@ export class Song {
     }
   }
 
-  public async makeResource(): Promise<AudioResource<Song> | void> {
+  public async makeResource(): Promise<AudioResource | void> {
     let playStream;
 
     const source = this.url.includes("youtube") ? "youtube" : "soundcloud";
 
     if (source === "youtube") {
-      playStream = await stream(this.url);
+      playStream = ytdl(this.url, { filter: "audioonly", liveBuffer: 0, quality: "lowestaudio" });
     }
 
     if (!stream) return;
 
-    return createAudioResource(playStream.stream, { metadata: this, inputType: playStream.type, inlineVolume: true });
+    if (!playStream) throw new Error("No stream found");
+
+    return createAudioResource(playStream, { metadata: this, inlineVolume: true });
   }
 
   public startMessage() {
